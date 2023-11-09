77cc81c85513a9c3fb62a4b138948f9f3cab114a
Erik <emauring@gmail.com>
2023-11-09 21:58:08 +0100

    Added another example with macro

--- read_environment_variables/src/main.rs
+++ read_environment_variables/src/main.rs
@@ -1,10 +1,13 @@
 use std::env;
 
 fn main() {
-let name = "USER";
+    let name: &'static str = "USER";
 
     match env::var(name) {
         Ok(v) => println!("{}: {}", name, v),
         Err(e) => panic!("${} is not set ({})", name, e)
     }
+
+    let user_env_variable = option_env!("USER").unwrap();
+    println!("{:?} is the current user", user_env_variable);
 }
